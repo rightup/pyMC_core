b6a7ba23430b4e8ee4da87abc31df5f0e4671d06
Lloyd <lloyd@rightup.co.uk>
2025-12-30 15:08:40 +0000

    Bump version to 1.0.6 and update related tests

--- src/pymc_core/hardware/lora/LoRaRF/__init__.py
+++ src/pymc_core/hardware/lora/LoRaRF/__init__.py
@@ -1,4 +1,5 @@
 # __init__.py
 from .base import BaseLoRa
 from .SX126x import SX126x
-from .SX127x import SX127x
+
+# from .SX127x import SX127x  # Commented out to avoid lgpio initialization when not using SX127x hardware
