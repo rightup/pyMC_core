8a3921da12246dc6754671e3187aac910eacd37a
Rightup <lloyd@rightup.co.uk>
2026-09-08 21:19:59 +0100

    fix(version): update version to 1.1.3

--- src/openhop_core/__init__.py
+++ src/openhop_core/__init__.py
@@ -3,7 +3,7 @@ openHop Core - A Python MeshCore library with SPI LoRa radio support
 Clean, simple API for building mesh network applications.
 """
 
-__version__ = "1.1.3.dev29"
+__version__ = "1.1.3"
 
 # Core mesh functionality
 from .node.node import MeshNode
